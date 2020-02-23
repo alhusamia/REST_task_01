e1a511de1ad4ef9a90bb1ed5976344d8db8e2857
PC <fdhfdh>
2020-02-23 14:33:58 +0200

    task one for rest

--- flights/views.py
+++ flights/views.py
@@ -1,1 +1,12 @@
 from .models import Flight, Booking
+from rest_framework.generics import ListAPIView
+from .serializers import FlightSerializer,BookingSerializer
+from datetime import datetime
+
+class HotelList(ListAPIView):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+class BookingList(ListAPIView):
+    queryset = Booking.objects.filter(date__gt=datetime.today()) # gt its for great than
+    serializer_class = BookingSerializer
